b515efa0e9ffa9f742fba811917940529a82c1c7
zzaain <sheriellaghzain@gmail.com>
2026-04-27 11:17:00 +0700

    menambahkan class orang

--- FriendClassSatu.cpp
+++ FriendClassSatu.cpp
@@ -2,3 +2,11 @@
 #include <string>
 using namespace std;
 
+class siswa; // Forward declaration
+class orang {
+private:
+    string nama;
+public:
+    void setNama(string pNama);
+    friend class siswa;
+};
