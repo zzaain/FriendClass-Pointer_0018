5feecf58ad0af686e8db72a5596209f77788f609
zzaain <sheriellaghzain@gmail.com>
2026-04-27 11:12:27 +0700

    menambahkan class mahasiswa

---
+++ pointerdua.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+class mahasiswa {
+public:
+    int nim;
+    void showNim() {
+        cout << "No Induk = " << nim << endl;
+    }
+};
+
+int main() {
+    
+}
