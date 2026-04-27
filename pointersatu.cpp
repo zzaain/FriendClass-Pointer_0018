5002a0bfa71711ddf1d5dbdf2ff2d5946803b27f
zzaain <sheriellaghzain@gmail.com>
2026-04-27 11:06:15 +0700

    menambahkan objek dan reference

--- pointersatu.cpp
+++ pointersatu.cpp
@@ -10,5 +10,16 @@ public:
 };
 
 int main() {
-    
+    mahasiswa mhs{1}; // Object mhs
+    mhs.showNim(); // Member Access Operator
+
+    mahasiswa &refMhs = mhs; // Pointer Reference refMhs
+    refMhs.nim = 2; // Member Access Operator
+    mhs.showNim();
+
+    mahasiswa *pMhs = &mhs; // Pointer Dereference pMhs
+    pMhs->nim = 3; // Arrow Operator
+    pMhs->showNim();
+
+    return 0;
 }
