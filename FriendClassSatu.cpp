e2f83a2207bf0ad90adb9872b85636c3657c4904
zzaain <sheriellaghzain@gmail.com>
2026-04-27 11:18:44 +0700

    menambahkan implement method

--- FriendClassSatu.cpp
+++ FriendClassSatu.cpp
@@ -18,3 +18,19 @@ public:
     void setId(int pId);
     void displayAll(orang &a);
 };
+
+void siswa::displayAll(orang &a) {
+    cout << id << endl << a.nama;
+}
+
+void orang::setNama(string pNama) {
+    nama = pNama;
+}
+
+void siswa::setId(int pId) {
+    id = pId;
+}
+
+int main() {
+    
+}
