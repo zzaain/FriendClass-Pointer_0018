dda0d5ab4001908c474d48dd6bd1989fb1d01f57
zzaain <sheriellaghzain@gmail.com>
2026-04-27 17:48:02 +0700

    menambahkan inputData pada LayangLayang

--- tugas.cpp
+++ tugas.cpp
@@ -10,6 +10,14 @@ private:
 double d1, d2;
 double sisi1, sisi2;
 
+public:
+void inputData() {
+cout << "Input Layang-Layang\n";
+cout << "Diagonal 1: "; cin >> d1;
+cout << "Diagonal 2: "; cin >> d2;
+cout << "Sisi 1: "; cin >> sisi1;
+cout 
+
 };
 
 
